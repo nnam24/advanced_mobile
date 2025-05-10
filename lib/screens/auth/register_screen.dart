a5092961e9e1bb3997a335939b2ba1aa71141a67
Le Nguyen Thai <21127162@student.hcmus.edu.vn>
2025-05-10 20:22:32 +0700

    update constraint register

--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -302,8 +302,8 @@ class _RegisterScreenState extends State<RegisterScreen> with SingleTickerProvid
                                     if (value == null || value.isEmpty) {
                                       return 'Please enter a password';
                                     }
-                                    if (value.length < 6) {
-                                      return 'Password must be at least 6 characters';
+                                    if (value.length < 8) {
+                                      return 'Password must be at least 8 characters';
                                     }
                                     return null;
                                   },
